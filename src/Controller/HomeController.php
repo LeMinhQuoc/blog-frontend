13641c241ea30d5ae9dcd9f61161e1a71a812cae
LeMinhQuoc <minhquoc7a3a.com>
2021-08-03 16:58:18 +0700

    update list blog

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,11 +1,9 @@
 <?php
-
 namespace App\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use GuzzleHttp\Client;
+use App\Helper\APIHelper;
 class HomeController extends AbstractController
 {
     /**
@@ -13,17 +11,14 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
-        $client = new Client([
-            'verify' => false,
-            'base_uri' => 'https://127.0.0.1:8001',
-        ]);
-        $response = $client->request('GET', '/api/blogs', [
+        $client = new APIHelper();
+        $response = $client -> get() -> request('GET', '/api/blogs', [
             'query' => [
                 'page' => '1',
             ]
-        ]);
-        $body = $response->getBody()->getContents();
+        ]);;
+        $body = $response -> getBody() -> getContents();
         $arr= json_decode($body,true);
-        return $this->render('home/index.twig', array('listBlog'=>$arr));
+        return $this -> render('home/index.twig', array('listBlog'=>$arr));
     }
 }
---
+++ src/Helper/APIHelper.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Helper;
+use GuzzleHttp\Client;
+//require_once "vendor/autoload.php";
+class APIHelper
+{
+    protected Client $client;
+    public function __construct(){
+        $this -> client = new Client([
+        'verify' => false,
+        'base_uri' => 'https://127.0.0.1:8001',
+        ]);
+    }
+    public function get() {
+        return $this -> client;
+    }
+}
